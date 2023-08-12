a042c3d5150f39ddb9f80a878a0c066e09545dc8
adityayfn <adhitya.firstian@gmail.com>
2023-08-12 14:48:35 +0700

    update config vuetify

--- src/vuetify.js
+++ src/vuetify.js
@@ -5,6 +5,7 @@ import * as directives from "vuetify/directives"
 import "@mdi/font/css/materialdesignicons.css"
 
 export default createVuetify({
+  ssr: true,
   components,
   directives,
   theme: {
@@ -14,5 +15,4 @@ export default createVuetify({
       },
     },
   },
- 
 })
